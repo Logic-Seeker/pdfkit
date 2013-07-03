cb068774947bea182ca00f922cc338cb206e4591
shobhab shrestha <shobhabstha@gmail.com>
2013-07-03 21:11:09 +0545

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfTest::Application.config.secret_token = 'a1b87cd9a71c11f005e26a32a86c8ad15d1dc25c8d4980b4a53154c24ac5a7048b7b4a64b768de59b3edaffad30743892e928efd4c792836920b1a75ec47c11b'
